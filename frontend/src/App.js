acea312aa7230a77f2aa9a144f0c8f47a93ad0ad
Baslael Workineh <baslaworku@gmail.com>
2024-03-06 18:25:18 +0300

    basic register done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from './components/Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
 function App() {
   return (
     <>
@@ -16,6 +19,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
 
   );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react"
 import { FaUser } from "react-icons/fa"
+import {useSelector, useDispatch} from 'react-redux'
+import { useNavigate } from "react-router-dom"
+import {toast} from 'react-toastify'
+import {register, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -10,6 +15,26 @@ function Register() {
     })
 
     const { name, email, password, password2 } = formData
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector(
+        (state) => state.auth
+    )
+
+    useEffect(()=>{
+        if(isError){
+            toast.error(message)
+        }
+
+        if(isSuccess || user){
+            navigate('/')
+        }
+
+        dispatch(reset());
+
+    },[user, isError, isSuccess, message, navigate, dispatch])
+
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
@@ -18,6 +43,20 @@ function Register() {
     }
     const onSubmit = (e) => {
         e.preventDefault()
+
+        if(password !== password2){
+            toast.error('Passwords do not match')
+        } else {
+            const userData = {
+                name, email, password,
+            }
+
+            dispatch(register(userData))
+        }
+    }
+
+    if(isLoading){
+        return <Spinner/>
     }
     return (
         <>
@@ -44,7 +83,7 @@ function Register() {
                     </div>
                     <div className="form-group">
                         <input type="password" className="form-control" id='password2'
-                            name='password2' value={password} placeholder="Confirm password" onChange={onChange} />
+                            name='password2' value={password2} placeholder="Confirm password" onChange={onChange} />
                     </div>
                     <div className="form-group">
                         <button type="submit" className="btn btn-block">
