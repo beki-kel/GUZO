372eb4d27d34082ea0473d9ae76a691bb74a8516
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-03-11 11:15:50 +0300

    some basic route on search done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Corrected route path: '/api/flights/search'
+app.use('/api/flights/serach', require('./routes/searchRoutes'));
 
 
-const app  = express()
-
-app.listen(port, ()=>console.log(`server started on ${port}`))
+app.listen(port, () => console.log(`Server started on ${port}`));
