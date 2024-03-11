c948764fcbcc4a8cba0fc5ed518db1134cab6b5e
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-03-11 10:00:30 +0300

    unnecessary deleted

--- backend/routes/goalRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getGoals, updatedGoal, setGoals, deleteGoals} = require('../controller/goalController')
-
-router.route('/').get(getGoals).post(setGoals)
-router.route('/:id').delete(deleteGoals).put(updatedGoal)
-
-
-module.exports = router
