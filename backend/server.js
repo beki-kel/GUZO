cfa4dbe08f4846122fb668e479998e1a561e96fe
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-03-11 10:43:32 +0300

    Basic Server Set up

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,8 @@
 const express = require('express')
-const donenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
+const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
-const colors = require('colors')
 
-connectDB()
 
 const app  = express()
-
-//this middleware is to use the req.body otherwise it will be undefined
-app.use(express.json())
-app.use(express.urlencoded({extended : false}))
-//.........................
-
-app.use('/api/goals', require('./routes/goalRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))
-
-app.use(errorHandler)
 
 app.listen(port, ()=>console.log(`server started on ${port}`))
