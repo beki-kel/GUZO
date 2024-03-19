2bdfa3c57e3e008722a4860e7ce3759b2f992d19
Baslael Workineh <baslaworku@gmail.com>
2024-03-19 20:20:55 +0300

    cleaned the frontend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Header from './components/Header';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
-    <>
-      <Router>
-        <div className='container'>
-          <Header />
-          <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </div>
-      </Router>
-      <ToastContainer />
-    </>
+    <div>
+      Hello World
+    </div>
 
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -11,9 +9,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
