45e328007dc508f0352cda583951952a3f504aaa
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-04-22 10:40:40 +0300

    login page done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
+import traveller4 from '../assets/traveller4.png';
 
 const Login = ({ handleLogin }) => {
   const navigate = useNavigate();
@@ -31,29 +34,44 @@ const Login = ({ handleLogin }) => {
   };
 
   return (
-    <div className='loginPage'>
-        <p className='Brand'>
-          <span className='firstSliceName'>Me</span>
-          <span className='secondSliceName'>he</span>
-          <span className='thirdSliceName'>ja</span>
-          <span className='domain'>.com</span>
-        </p>
+    <div className='flex justify-center items-center bg-white w-full min-h-screen  px-10'>
+  <div className='flex flex-col mb-10 text-3xl font-thin w-1/2 items-center justify-center'>
+    <div className='flex w-full justify-center items-center mb-6'>
+      <FontAwesomeIcon icon={faPlaneDeparture} style={{ color: "#ff622e" }} className='mr-2' />
+      <h2 className='text-4xl font-medium'>Exopia</h2>
+    </div>
+    
+    <div className='flex flex-col w-full text-center justify-center items-center'>
+      <h4 className='w-full text-orange-600 font-normal'>Discover Your Adventure with a Web all in one</h4>
+      <p className='w-full text-xl'> book your flights,rides,dinning,events and more!</p>
+      <p className='w-full text-xl'> Ethiopia the land of origins.</p>
+    </div>
+      
+  </div>
 
-      <form onSubmit={handleSubmit} className='registerForm'>
-      {errorMessage && <div className='errorDisplay'>{errorMessage}</div>}
-        <div className='loginField'>
-          <label>Username:</label>
-          <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className='loginInputField' />
-        </div>
-        <div className='loginField'>
-          <label>Password:</label>
-          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className='loginInputField'/>
-        </div>
-        <button type="submit" className='buttons'>Login</button>
-        <Link to="/register" className='anchor'>
-          <button className='buttons'> Signup</button>
-        </Link>
-      </form>
+      <div className='flex flex-col justify-center items-center mt-auto w-1/2'>
+        <form onSubmit={handleSubmit} className="bg-black px-8 py-12 rounded-3xl shadow-lg w-full max-w-sm border-2 border-orange-600 justify-center items-center">
+      <h1 className="text-2xl text-center text-white mb-4 font-bold">Login</h1>
+        {errorMessage && <div className='errorDisplay'>{errorMessage}</div>}
+          <div className='flex justify-center items-center space-x-5 w-full'>
+            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className='rounded-lg p-2 mb-3 text-center bg-gray-900 focus:outline-none focus:border-none focus:outline-orange-600 text-white w-full' />
+          </div>
+          <div className='flex justify-center items-center space-x-5 w-full'>
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className='rounded-lg p-2 mb-3 text-center bg-gray-900 focus:outline-none focus:border-none focus:outline-orange-600 text-white w-full'/>
+          </div>
+          <div className='flex flex-col w-full items-center justify-center'>
+            <button type="submit" className="bg-orange-600 text-white font-bold py-2 px-4 mt-6 rounded-xl w-8/12">Login</button>
+            <div className="flex flex-col w-full items-center justify-center">
+              <p className="text-white font-medium mt-2">don't have account? <Link to='/register' className='text-orange-600 underline font-normal'>Signup </Link></p>
+            </div>
+          </div>
+        </form>
+
+      <div className='flex w-full h-72 justify-end'>
+        <img src={traveller4} alt="traveller"  className="object-contain h-full"/>
+      </div>
+      </div>
+
     </div>
   );
 };
