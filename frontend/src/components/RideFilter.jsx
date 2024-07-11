9eda52871aff576781d9b4b26762a566b5ccc930
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-11 15:33:39 +0300

    feat: Add RideFilter component

---
+++ frontend/src/components/RideFilter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RideFilter({rideResponse,rideLoading,rideError}) {
+    return (
+        <div>
+        
+        </div>
+    )
+    }
+
+export default RideFilter
