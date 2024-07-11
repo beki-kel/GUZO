7e586bc9dfdccc5c844a4a1e3040ae27c194ba04
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-12 01:31:41 +0300

    feat: Add lazy loading for images in RideFilter component

--- frontend/src/components/RideFilter.jsx
+++ frontend/src/components/RideFilter.jsx
@@ -1,19 +1,58 @@
-import React from 'react'
+import React from 'react';
+import orangeLoading from '../assets/orange-gif.gif';
+import blackCar from '../assets/black-sedan-car-isolated-white-vector.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-function RideFilter({rideResponse,rideLoading,rideError}) {
+const RideFilter = ({ rideResponse, rideLoading, rideError }) => {
     return (
         <div className='w-full my-10 flex flex-col'>
             {rideLoading && (
-                <div  className='w-full flex justify-center items-center mb-2 p-2'>
+                <div className='w-full flex justify-center items-center mb-2 p-2'>
                     <img src={orangeLoading} alt="Loading" className="w-50 h-20 rounded-t-lg" />
                 </div>
             )}
 
-            { rideError && (
+            {rideError && (
                 <div className='w-full text-center text-2xl text-red-600 p-9'>{rideError}</div>
             )}
-        </div>
-    )
-    }
 
-export default RideFilter
+            {rideResponse && (
+                <p className="text-3xl font-serif w-full text-center text-gray-800  mb-4">Found results for Rides</p>
+            )}
+
+            {rideResponse && rideResponse.map((ride) => (
+                <div key={ride._id} className='w-[45%] p-3 m-4 rounded-3xl shadow-md shadow-gray-300 hover:scale-110 transform transition duration-700'>
+                    <div className='bg-gradient-to-br from-red-400 to-orange-300 py-2 px-3 m-3 rounded-xl shadow-lg shadow-orange-600 z-20 flex flex-col'>
+                        <div className='w-full h-7/12 flex items-center justify-center'>
+                            <img className='w-1/2 h-7/12 z-30' src={blackCar} alt="Car" />
+                            <div className='w-1/2 h-7/12 flex items-center justify-center z-30'>
+                                <div className='w-20 h-20 rounded-full flex items-center justify-center bg-gray-200 border-4 border-white shadow-orange-700 shadow-xl'>
+                                    <FontAwesomeIcon icon={faUser} className='w-12 h-12 text-black' />
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className='w-full h-5/12 mt-6 flex pb-2'>
+                            <div className='w-1/2'>
+                                <p className='font-semibold text-white'> Full Name <span className='text-black ml-1 font-mono'>{ride.drivername}</span></p>
+                                <p className='font-semibold text-white'> Car Brand <span className='text-black ml-1 font-mono'>{ride.brand}</span></p>
+                                <p className='font-semibold text-white'> Car Rating <span className='text-black ml-1 font-mono'>{ride.totalratings}</span></p>
+                                <p className='font-semibold text-white'> Plate No <span className='text-black ml-1 font-mono'>{ride.plateNo}</span></p>
+                            </div>
+
+                            <div className='w-1/2'>
+                                <p className='font-semibold text-white'> Capacity <span className='text-black font-mono'>{ride.capacity}</span></p>
+                                <p className='font-semibold text-white'> Driver Rating <span className='text-black font-mono'>{ride.driverrating}</span></p>
+                                <p className='font-semibold text-white'> Driver Location <span className='text-black font-mono'>{ride.pickUp}</span></p>
+                                <p className='font-semibold text-black'><span className='text-black font-bold text-xl font-mono'>{ride.finalPrice.toFixed(2)} </span>birr</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default RideFilter;
