c1bb669d5bc9ac515afa7234eec70e8279169c5e
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-11 15:38:55 +0300

    feat: Add loading and error handling to RideFilter component

--- frontend/src/components/RideFilter.jsx
+++ frontend/src/components/RideFilter.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function RideFilter({rideResponse,rideLoading,rideError}) {
     return (
-        <div>
-        
+        <div className='w-full my-10 flex flex-col'>
+            {rideLoading && (
+                <div  className='w-full flex justify-center items-center mb-2 p-2'>
+                    <img src={orangeLoading} alt="Loading" className="w-50 h-20 rounded-t-lg" />
+                </div>
+            )}
+
+            { rideError && (
+                <div className='w-full text-center text-2xl text-red-600 p-9'>{rideError}</div>
+            )}
         </div>
     )
     }
