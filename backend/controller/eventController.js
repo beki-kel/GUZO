d0c4be0b5015a4d352b13eae2caa86e264352132
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-12 17:00:43 +0300

    feat: Add searchEvent and addEvent functions to eventController

---
+++ backend/controller/eventController.js
@@ -0,0 +1,45 @@
+const Event = require('../models/EventsModel');
+
+const searchEvent = async (req, res ) =>{
+    try{
+        const query = req.body.title;
+        const category = req.body.category;
+        const date = req.body.date;
+
+        const event = await Event.find({
+            ...(query && { title: { $regex: query, $options: 'i' } }),
+            ...(category && { category }),
+            ...(date && { date }),
+            ...(location && { location }),
+            ...(description && { description }),
+        });
+        res.json(event);
+    }catch (err){
+        console.error(err)
+        res.status(500).json({ error: err.message || 'Internal Server Error' });
+    }
+}
+
+const addEvent = async (req,res) =>{
+    const { title, category, date, location, description } = req.body;
+
+    const newEvent = new Event({
+        title,
+        category,
+        date,
+        location,
+        description,
+    });
+
+    try{
+        const savedEvent = await newEvent.save();
+        res.status(201).json(savedEvent);
+    }catch(err){
+        res.status(400).json({ message: err.message });
+    }
+}
+
+module.exports = {
+    searchEvent,
+    addEvent
+}
