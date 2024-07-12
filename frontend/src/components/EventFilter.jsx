41e07a0fa9f18aab64e3d0fff02ed852ba61035c
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-13 00:01:02 +0300

    refactor: Add lazy loading for images in EventFilter component

--- frontend/src/components/EventFilter.jsx
+++ frontend/src/components/EventFilter.jsx
@@ -1,27 +1,55 @@
-import React from 'react'
+import React from 'react';
 import orangeLoading from '../assets/orange-gif.gif';
+import Kebero from '../assets/Kebero.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faLocationDot, faStar } from '@fortawesome/free-solid-svg-icons';
+import HoverCard from "@darenft/react-3d-hover-card";
+import "@darenft/react-3d-hover-card/dist/style.css";
 
-function EventFilter({eventResponse , eventLoading , eventError}) {
+function EventFilter({ eventResponse, eventLoading, eventError }) {
     return (
         <div>
             <div className='w-full my-10 flex flex-col'>
                 {eventLoading && (
                     <div className='w-full flex justify-center items-center mb-2 p-2'>
-                        <img src={orangeLoading} alt="Loading" className="w-50 h-20 rounded-t-lg" />
+                        <img src={orangeLoading} alt="Loading" className="w-50 h-20 rounded-t-lg " />
                     </div>
                 )}
 
-                { eventError && (
+                {eventError && (
                     <div className='w-full text-center text-2xl text-red-600 p-9'>{eventError}</div>
                 )}
 
-                {eventResponse  && (
-                    <p className="text-3xl font-serif w-full text-center text-gray-800  mb-4">Found results for Events</p>
+                {eventResponse && (
+                    <div className='w-full my-10 flex flex-col'>
+                        <p className="text-3xl font-serif w-full text-center text-gray-800">Found results for Events</p>
+                        <div className='w-full mb-36 flex flex-wrap items-center justify-center'>
+                            {eventResponse.map((event, index) => (
+                                <div key={index} className='w-1/4 mx-3 mt-36 flex flex-col justify-center items-center bg-gradient-to-br from-red-400 to-orange-300 rounded-2xl shadow-2xl hover:scale-105 text-black'>
+                                    <HoverCard scaleFactor={1}>
+                                        <div className='w-full mt-2 text-black px-6 py-3'>
+                                            <img src={Kebero} className='w-full h-56 rounded-t-md mb-8 z-30 shadow-xl rounded-lg mt-[-6rem]' alt="Event" />
+                                        </div>
+                                    </HoverCard>
+                                    <p className='mr-auto ml-2 text-lg text-center p-2'>{event.description}</p>
+                                    <div className='w-full flex items-center justify-center px-3 py-2'>
+                                        <FontAwesomeIcon icon={faLocationDot} className='h-4 ml-2 text-white' />
+                                        <p className='mr-auto ml-2 text-lg'>{event.location}</p>
+                                        <FontAwesomeIcon icon={faCalendar} className='h-4 ml-2 text-white' />
+                                        <p className='mr-auto ml-2 text-lg'>{event.date}</p>
+                                    </div>
+                                    <div className='w-full p-3 bg-gray-50 text-black rounded-b-2xl flex items-center justify-center mt-2'>
+                                        <p className='mr-auto ml-2 text-lg'>{event.title}</p>
+                                        <FontAwesomeIcon icon={faStar} className="text-black" />
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 )}
-
             </div>
         </div>
-    )
+    );
 }
 
-export default EventFilter
+export default EventFilter;
