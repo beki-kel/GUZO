46f6a7b35003c764b9970b70932f5926feddd5d4
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-13 23:45:58 +0300

    refactor: Add HomePricingCard component and fetch trending packages

---
+++ frontend/src/components/HomePricingCard.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Placeholder } from 'rsuite';
+import PricingCards from './pricingCard';
+import cardImage from '../assets/wenchi.jpg';
+
+function HomePricingCard() {
+    const [cardData, setCardData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        const fetchCards = async () => {
+            const trending = true;
+            try {
+                const response = await axios.post('http://localhost:5000/search/packages', { trending });
+                const data = response.data.packages;
+                console.log(data);
+                setCardData(data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            }
+        };
+        fetchCards();
+    }, []);
+
+    const cardDataReal = (cardData, cardImage) => {
+        return cardData.map((card) => ({
+            title: card.name,
+            features: card.includes,
+            price: card.price,
+            image: cardImage
+        }));
+    };
+
+    return (
+        <div className='w-full flex flex-col justify-center items-center my-5'>
+            <p className="text-4xl font-serif font-medium text-orange-600 w-full text-center mb-6">Our Trending Packages</p>
+            {loading && (
+                <div className='w-full flex p-10'>
+                    <Placeholder.Graph active />
+                </div>
+            )}
+            {!loading && cardData.length > 0 && <PricingCards cardData={cardDataReal(cardData, cardImage)} />}
+
+            <button className='px-10 py-6 border-none rounded-2xl text-white text-lg font-medium  bg-orange-500 mt-[-6rem] mb-10 hover:opacity-85'> Create Your Own Package </button>
+        </div>
+    );
+}
+
+export default HomePricingCard;
