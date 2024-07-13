eb1d029e24b5647115e6d59043f2c9202a8115aa
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-13 23:46:30 +0300

    refactor: Update pricing card styles and add lazy loading for images

--- frontend/src/components/pricingCard.jsx
+++ frontend/src/components/pricingCard.jsx
@@ -6,23 +6,23 @@ import { Link } from "react-router-dom";
 const PricingCards = (props) => {
     const cardData= props.cardData
   return (
-    <div className="w-full pt-[8rem] pb-10 px-4  ">
+    <div className="w-full pt-[8rem] pb-10 px-4 ">
       <div className="max-w-[1000px] mx-auto grid md:grid-cols-3 gap-8">
         {cardData.map((card, index) => (
-          <div key={index} className={`w-full shadow-sm flex flex-col p-4 my-4 rounded-3xl hover:scale-105 duration-300   shadow-orange-600`}>
-            <img className="w-10/12 h-44 rounded-t-md mx-auto mt-[-8rem] bg-white" src={card.image} alt="/"
+          <div key={index} className={`w-full shadow-sm flex flex-col p-4 mb-28 rounded-3xl hover:scale-105 duration-300   shadow-orange-600 bg-gradient-to-br from-red-400 to-orange-300`}>
+            <img className="w-10/12 h-44 rounded-t-xl mx-auto mt-[-8rem] bg-white" src={card.image} alt="/"
             />
-            <h2 className="text-xl  font-medium text-center pt-4 ">
+            <h2 className="text-2xl text-black  font-medium text-center pt-4 ">
               {card.title}
             </h2>
-            <p className="text-center text-orange-600 text-3xl font-medium pt-2">{card.price}</p>
-            <div className="justify-center items-center font-medium">
+            <p className="text-center text-white text-3xl font-medium pt-2">{card.price} Birr</p>
+            <div className="justify-center items-center font-medium ">
               {card.features.map((feature, index) => (
                 
                 <p
                   key={index}
-                  className={`py-2 mx-8 border-b items-center justify-center ${index === 0 ? "mt-8" : ""}`}>
-                    <FontAwesomeIcon icon={faCheck} className="text-[#ff6d2e] mr-2 items-center justify-center" />
+                  className={`py-2 mx-6 border-b text-lg text-black items-center justify-center ${index === 0 ? "mt-8" : ""}`}>
+                    <FontAwesomeIcon icon={faCheck} className="text-white mr-2 items-center justify-center" />
                   {feature}
                 </p>
               ))}
@@ -30,7 +30,7 @@ const PricingCards = (props) => {
             
             <Link to='/login' className="flex justify-center items-center w-full">
                 <button
-                className={`bg-orange-500 text-white hover:bg-orange-600 hover:opacity-2 duration-150 w-[130px] rounded-md font-medium my-6 mx-auto px-6 py-2`}>
+                className={`bg-orange-600 text-white hover:bg-orange-700 hover:opacity-2 duration-150 w-[130px] rounded-md font-medium my-6 mx-auto px-6 py-2`}>
                 Book now
                 </button>
             </Link>
