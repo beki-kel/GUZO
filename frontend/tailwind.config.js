e65eb62a9dc857c68a6685dd2099660ce7c635d2
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-14 12:59:12 +0300

    refactor: Update tailwind.config.js with Material Tailwind integration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports =  withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -12,4 +13,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+});
