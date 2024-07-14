50f93540ade90f5a5fa3a0a567df5fb9038ede07
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-14 12:59:53 +0300

    refactor: Update pricingCard component with improved image styles and lazy loading

--- frontend/src/components/pricingCard.jsx
+++ frontend/src/components/pricingCard.jsx
@@ -9,7 +9,7 @@ const PricingCards = (props) => {
     <div className="w-full pt-[8rem] pb-10 px-4 ">
       <div className="max-w-[1000px] mx-auto grid md:grid-cols-3 gap-8">
         {cardData.map((card, index) => (
-          <div key={index} className={`w-full shadow-sm flex flex-col p-4 mb-28 rounded-3xl hover:scale-105 duration-300   shadow-orange-600 bg-gradient-to-br from-red-400 to-orange-300`}>
+          <div key={index} className={`w-full shadow-sm flex flex-col p-4 mb-28 rounded-3xl hover:scale-105 duration-300   shadow-orange-600 bg-gradient-to-br from-red-300 to-orange-200`}>
             <img className="w-10/12 h-44 rounded-t-xl mx-auto mt-[-8rem] bg-white" src={card.image} alt="/"
             />
             <h2 className="text-2xl text-black  font-medium text-center pt-4 ">
@@ -30,7 +30,7 @@ const PricingCards = (props) => {
             
             <Link to='/login' className="flex justify-center items-center w-full">
                 <button
-                className={`bg-orange-600 text-white hover:bg-orange-700 hover:opacity-2 duration-150 w-[130px] rounded-md font-medium my-6 mx-auto px-6 py-2`}>
+                className={`bg-orange-800 text-white hover:bg-orange-700 hover:opacity-2 duration-150 w-[130px] rounded-md font-medium my-6 mx-auto px-6 py-2`}>
                 Book now
                 </button>
             </Link>
