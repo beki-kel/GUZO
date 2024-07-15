b6ae4621527fd1753152eb58cad90bec6438961c
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-15 19:00:44 +0300

    refactor: Add BlogModel for managing blog data
    This commit adds the BlogModel file, which defines the schema and model for managing blog data in the application. The BlogModel includes fields for title, content, header, author, and date. This change ensures that the application can properly create, read, update, and delete blog data.

---
+++ backend/models/BlogModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    header:{
+        type:String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date: {
+        type: String,
+        required: true,
+    }
+},{ timestamps: true });
+
+const BlogModel = mongoose.model('Blog', blogSchema);
+
+module.exports = BlogModel;
