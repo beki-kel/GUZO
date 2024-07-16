5cbca482d99a5399a6229a6613cb802faf2db287
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-17 00:21:49 +0300

    refactor: Handle duplicate key errors and validation errors in authRegisterController.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,49 +1,33 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-// Define the user schema
-const userSchema = new mongoose.Schema({
-    fname:{
-        type: String,
-        required: true
-    },
-    lname:{
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
-    isSubscribed:{
-        type: Boolean,
-        default: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-},
-    {timestamps : true});
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+    minlength: [3, 'Username must be at least 3 characters long'],
+  },
+  fname: {
+    type: String,
+    required: [true, 'First name is required'],
+  },
+  lname: {
+    type: String,
+    required: [true, 'Last name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters long'],
+  },
+}, { timestamps: true });
 
-//don't change the 'User' name cause i have used it in the notificationsModel
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
