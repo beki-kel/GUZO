189792e7b886c489ca2ea989a40d84a186088371
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-16 14:59:12 +0300

    refactor: Update allowedHeaders in CORS configuration to include 'Access-Control-Allow-Origin'

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ const corsOptions = {
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
 };
 
 app.use(cors(corsOptions));
