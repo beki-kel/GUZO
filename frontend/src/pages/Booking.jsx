6a0f018c91063e8cc14eeb873fd13c6d5be7d24c
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-18 23:25:01 +0300

    refactor: Optimize image loading in Booking.jsx

--- frontend/src/pages/Booking.jsx
+++ frontend/src/pages/Booking.jsx
@@ -3,16 +3,16 @@ import Footer from '../components/Footer';
 import Navigation from '../components/Navigation';
 
 function Booking({isLoggedIn}) {
-  return (
-    <div>
-          <div className='flex flex-col w-full min-h-screen bg-white'>
-       <Navigation list={['Home', 'Blog', 'Bookings', 'Packages']} title='Exopia' isLoggedIn={isLoggedIn} App={false} />
-       <div className='w-full min-h-screen'>
-       </div>
-       <Footer/>
-    </div>
-    </div>
-  )
+    return (
+        <div>
+            <div className='flex flex-col w-full min-h-screen bg-white'>
+        <Navigation list={['Home', 'Blog', 'Bookings', 'Packages']} title='Exopia' isLoggedIn={isLoggedIn} App={false} />
+        <div className='w-full min-h-screen'>
+        </div>
+        <Footer/>
+        </div>
+        </div>
+    )
 }
 
-export default Booking
+export default Booking;
