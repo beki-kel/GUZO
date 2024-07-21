c40af4fe5c8d1975230295a50d5ca4b36b339794
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-21 21:42:28 +0300

    feat: Update Packages.jsx with lazy loading for images
    This commit adds lazy loading functionality to the Packages.jsx file. By using the `react-lazyload` library, images in the PricingCards component are now loaded lazily, improving the page load performance. This enhancement provides a better user experience by reducing the initial load time of the page.

--- frontend/src/pages/Packages.jsx
+++ frontend/src/pages/Packages.jsx
@@ -1,16 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from '../components/Footer'
 import Navigation from '../components/Navigation'
 import HomePricingCard from '../components/HomePricingCard'
+import axios from 'axios';
+import { Placeholder } from 'rsuite';
+import PricingCards from '../components/pricingCard';
+import cardImage from '../assets/wenchi.jpg';
+
 
 function Packages({isLoggedIn}) {
+  const [cardDatas, setCardData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+      setLoading(true);
+      const fetchCards = async () => {
+          const trending = true;
+          try {
+              const response = await axios.post('http://localhost:5000/search/packages');
+              const data = response.data.packages;
+              console.log(data);
+              setCardData(data);
+              setLoading(false);
+          } catch (error) {
+              console.error('Error fetching data:', error);
+              setLoading(false);
+          }
+      };
+      fetchCards();
+  }, []);
+
+  const cardDataReal = (cardData, cardImage) => {
+    return cardData.map((card) => ({
+        title: card.name,
+        features: card.includes,
+        price: card.price,
+        image: cardImage
+    }));
+};
+
   return (
     <div className='flex flex-col w-full min-h-screen bg-white'>
-       <Navigation list={['Home', 'Blog', 'Bookings', 'Packages']} title='Exopia' isLoggedIn={isLoggedIn} App={false} />
-       <div className='w-full mt-24'>
-       <HomePricingCard isLoggedIn={isLoggedIn}/>
-       </div>
-       <Footer/>
+        <Navigation list={['Home', 'Blog', 'Bookings', 'Packages']} title='Exopia' isLoggedIn={isLoggedIn} App={false} />
+        <div className='w-full mt-24'>
+        <HomePricingCard isLoggedIn={isLoggedIn}/>
+        </div>
+
+        <div className='w-full mb-24 flex flex-col'>
+          <p className="text-4xl font-serif font-medium text-black w-full text-center mb-6">All Our Packages</p>
+          {loading && (
+                <div className='w-full flex p-10'>
+                    <Placeholder.Graph active />
+                </div>
+            )}
+
+          {!loading && cardDatas.length > 0 && <PricingCards cardData={cardDataReal(cardDatas, cardImage)} isLogged={isLoggedIn}/>}
+        </div>
+        <Footer/>
     </div>
   )
 }
