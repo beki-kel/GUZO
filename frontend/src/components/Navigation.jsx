a7f605445f7361cb34200139d839534fbac05c76
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-21 21:41:22 +0300

    feat: Update Navigation component with partner and notification links
    This commit updates the Navigation component to include partner and notification links. It removes the "Become a Partner" and "Notification" list items that were previously hardcoded and replaces them with dynamic links. This enhancement provides a more flexible and scalable navigation experience for users, allowing them to easily access the partner and notification sections of the application.

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -94,24 +94,6 @@ const handleLogout = () => {
                       <span className='text-orange-800 font-serif text-xl'>Profile</span>
                     </ListItem>
 
-                    <ListItem >
-                      <ListItemPrefix className='text-black h-8 w-8'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                          <path fillRule="evenodd" d="M7.5 3.75A1.5 1.5 0 0 0 6 5.25v13.5a1.5 1.5 0 0 0 1.5 1.5h6a1.5 1.5 0 0 0 1.5-1.5V15a.75.75 0 0 1 1.5 0v3.75a3 3 0 0 1-3 3h-6a3 3 0 0 1-3-3V5.25a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3V9A.75.75 0 0 1 15 9V5.25a1.5 1.5 0 0 0-1.5-1.5h-6Zm10.72 4.72a.75.75 0 0 1 1.06 0l3 3a.75.75 0 0 1 0 1.06l-3 3a.75.75 0 1 1-1.06-1.06l1.72-1.72H9a.75.75 0 0 1 0-1.5h10.94l-1.72-1.72a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                        </svg>
-                      </ListItemPrefix>
-                      <span className='text-orange-800 text-xl font-serif'>Become a Partner</span>
-                    </ListItem>
-
-                    <ListItem>
-                      <ListItemPrefix className='text-black h-8 w-8'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                          <path fillRule="evenodd" d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z" clipRule="evenodd" />
-                        </svg>
-                      </ListItemPrefix>
-                      <span className='text-orange-800 text-xl font-serif'>Notification</span>
-                    </ListItem>
-
                     <ListItem onClick={handleLogout}>
                       <ListItemPrefix className='text-black'>
                         <PowerIcon className="h-8 w-8" />
